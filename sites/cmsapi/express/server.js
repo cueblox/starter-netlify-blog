c7afcda303c84d7d9c476f80efb2987b3b765208
Brian Ketelsen <bketelsen@gmail.com>
2021-04-27 14:00:09 -0400

    redirects

--- sites/cmsapi/express/server.js
+++ sites/cmsapi/express/server.js
@@ -9,8 +9,8 @@ const fetch = require('sync-fetch');
 
 const data = fetch('https://github.com/cueblox/starter-netlify-blog/releases/download/blox/data.json').json();
 const router = jsonServer.router(data, { foreignKeySuffix: '_id' })
-app.use('/.netlify/functions/server/graphql', jsonGraphqlExpress(data));
-app.use("/.netlify/functions/server", router);
+app.use('/graphql', jsonGraphqlExpress(data));
+app.use("/api", router);
 
 
 module.exports = app;
