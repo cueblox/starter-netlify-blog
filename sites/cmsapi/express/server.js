1c4ab66ea992f24d3fee808b44a4e4ddb7ffce36
Brian Ketelsen <bketelsen@gmail.com>
2021-04-27 13:26:43 -0400

    graphql

--- sites/cmsapi/express/server.js
+++ sites/cmsapi/express/server.js
@@ -7,7 +7,7 @@ const serverless = require('serverless-http');
 const app = express();
 const fetch = require('sync-fetch');
 
-const data = fetch('https://github.com/cueblox/starter-azure-blog/releases/download/blox/data.json').json();
+const data = fetch('https://github.com/cueblox/starter-netlify-blog/releases/download/blox/data.json').json();
 const router = jsonServer.router(data, { foreignKeySuffix: '_id' })
 app.use('/.netlify/functions/server/graphql', jsonGraphqlExpress(data));
 app.use("/.netlify/functions/server", router);
